7e35668cb7141133d52c7c8bb88cc2c43c5dd5eb
jw-albert <ru04jo30801@gmail.com>
2025-10-27 19:59:26 +0800

    Translate comments to traditional Chinese
    Updated comments to use traditional Chinese language.

--- Lab2/Q2/Q2.c
+++ Lab2/Q2/Q2.c
@@ -7,7 +7,6 @@
 int main() {
     pid_t pid;
 
-    // 建立三個子程序
     for (int i = 0; i < 3; i++) {
         pid = fork();
 
@@ -16,17 +15,15 @@ int main() {
             exit(1);
         } 
         else if (pid == 0) {
-            // 子程序執行 programA
             execlp("./programA", "programA", NULL);
             perror("execlp failed");
             exit(1);
         }
-        // 父程序不做任何事，直接繼續下一個迴圈
+        // 父行程不做事，繼續下一圈
     }
 
-    // 父程序等待所有子程序結束
+    // 父行程等待所有子行程結束
     while (wait(NULL) > 0);
-
     printf("I'm parent\n");
 
     return 0;
