a1e25fd62618216e26726addba314b27ddea32b2
jw-albert <ru04jo30801@gmail.com>
2025-10-27 20:23:03 +0800

    Refactor main function to use fork and exec
    Refactor client socket code to implement process forking and execute a separate program.

--- Lab2/Q2/Q2.c
+++ Lab2/Q2/Q2.c
@@ -1,59 +1,28 @@
-#include #include #include #include #include #include #include #include
-#define PORT 4444
-#define IP_ADDRESS "127.0.0.1"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main()
-{
-
-int clientSocket;
-struct sockaddr_in serverAddr;
-int checkConnect, checkRecv;
-char buffer[1024];
-
-/* replace this line with socket() */
-if (/* check error */)
-{
-printf("[-]Error in creating Client Socket.\n");
-exit(1);
-}
-printf("[+]Client Socket is created.\n");
-
-memset(&serverAddr, '\0', sizeof(serverAddr));
-serverAddr.sin_family = AF_INET;
-serverAddr.sin_port = htons(PORT);
-serverAddr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
-
-/* replace this line with connect() */
-if (/* check error */)
-{
-printf("[-]Error in connection.\n");
-exit(1);
-}
-printf("[+]Connected to Server.\n");
-
-while (1)
-{
-printf("Client : ");
-scanf("%s", &buffer[0]);
-/* replace this line with send() */
-
-if (strcmp(buffer, ":exit") == 0)
-{
-close(clientSocket);
-printf("[-]Disconnected from server.\n");
-exit(1);
-}
-
-/* replace this line with recv() */
-if (/* check error */)
-{
-printf("[-]Error in receiving data.\n");
-}
-else
-{
-printf("Server : %s\n", buffer);
-}
-}
-
-return 0;
+int main() {
+  pid_t pid;
+  for(int i = 0; i < 3; i++){
+    pid = fork();
+  }
+  while(wait(NULL) > 0);
+  if(pid > 0)
+  {
+    printf("I'm parent\n");
+  }
+  if(pid == 0){
+    if(execlp("./programA","programA.c",NULL) != 0)
+      {
+        perror("execlp failed\n");
+        exit(1);
+      }
+    }
+  if(pid < 0){
+    perror("fork failed");
+    exit(1);
+  }
+  return 0;
 }
