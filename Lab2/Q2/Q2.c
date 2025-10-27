c6d48af05560be4a6ced90b3a92f2c59ef907b5b
Andy <154674108+05zhi@users.noreply.github.com>
2025-10-27 20:16:17 +0800

    Update Q2.c

--- Lab2/Q2/Q2.c
+++ Lab2/Q2/Q2.c
@@ -1,42 +1,35 @@
-#include <stdio.h> /* printf */
-#include <sys/types.h> /* pid_t */
-#include <unistd.h> /* fork(), getpid() */
-#include <stdlib.h> /* exit() */
-#include <sys/wait.h> /* wait() */
-//execlp(const char *file, const char *arg, ..., NULL);
-//execlp(尋找程式路徑, 傳遞給程式的第一個引數(通常為程式名稱), ..., NULL);
-//該程式可利用argv[0]取得程式名稱
+#include <stdio.h>      // printf
+#include <sys/types.h>  // pid_t
+#include <unistd.h>     // fork(), getpid(), execlp()
+#include <stdlib.h>     // exit()
+#include <sys/wait.h>   // wait()
 
 int main() {
     pid_t pid;
 
-    /* 建立4個 child（父進程每次 fork，子 process 跳出迴圈） */
-    for(int i = 0; i < 4; i++)
-    {
+    /* 建立4個 child process */
+    for (int i = 0; i < 4; i++) {
         pid = fork();
-        if (pid == 0) {
-            /* 子程序跳出迴圈，避免繼續 fork */
-            break;
+
+        if (pid < 0) {
+            perror("fork failed");
+            exit(1);
+        } 
+        else if (pid == 0) {
+            // 子行程：執行 programA
+            printf("Child [%d] executing programA...\n", getpid());
+
+            if (execlp("./programA", "programA", NULL) == -1) {
+                perror("execlp failed");
+                exit(1);
+            }
         }
-        /* 父程序則繼續下一次迴圈以產生更多 child */
+        // 父行程繼續迴圈建立下一個子行程
     }
 
-    /* 父進程等待所有子程序結束 */
-    while(wait(NULL) > 0);
+    // 父行程等待所有子行程結束
+    while (wait(NULL) > 0);
 
-    if(pid == 0)
-    {
-        /* 在子程序中用 execlp 跳到 programA 執行 */
-        if (execlp("./programA", "programA", NULL) == -1)  /*利用execlp使process跳到別的程式執行*/
-        {                               /*若execlp執行失敗，回傳-1*/
-            perror("execlp failed"); /*印出錯誤資訊*/
-            exit(1);   
-        }
-    }
-    else
-    {
-        perror("fork failed");
-        exit(1);
-    }
+    printf("Parent [%d] all children finished.\n", getpid());
     return 0;
 }
