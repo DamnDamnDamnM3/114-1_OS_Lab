3657c816c778657955c35d016891f0ad504719b2
Andy <154674108+05zhi@users.noreply.github.com>
2025-10-27 20:20:26 +0800

    Update Q2.c

--- Lab2/Q2/Q2.c
+++ Lab2/Q2/Q2.c
@@ -1,27 +1,29 @@
-#include <stdio.h>      // printf
-#include <sys/types.h>  // pid_t
-#include <unistd.h>     // fork(), getpid(), execlp()
-#include <stdlib.h>     // exit()
-#include <sys/wait.h>   // wait()
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
 
 int main() {
     pid_t pid;
 
-    /* 建立4個 child process */
     for (int i = 0; i < 4; i++) {
         pid = fork();
-
         if (pid < 0) {
             perror("fork failed");
             exit(1);
-        } 
-        else if (pid == 0) {
+        }
+
+        if (pid == 0) {
+            // 子行程執行 programA
             if (execlp("./programA", "programA", NULL) == -1) {
                 perror("execlp failed");
                 exit(1);
             }
+        } else {
+            // 父行程印出
+            printf("I'm parent\n");
         }
-        // 父行程繼續迴圈建立下一個子行程
     }
 
     // 父行程等待所有子行程結束
